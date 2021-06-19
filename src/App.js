33a026de2095c02e98ec088302af362561a4aac2
csolis1 <celida.solis@outlook.com>
2021-06-19 14:09:47 -0400

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
+import Header from './Components/Header';
+import Score from './Components/Score'
+import Instructions from './Components/Instructions'
+import GameBoard from './Components/GameWindow'
+import Reset from './Components/Reset'
+import Footer from './Components/Footer';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Score/>
+      <Instructions/>
+      <GameBoard/>
+      <Reset/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Components/GameWindow.js
@@ -0,0 +1,8 @@
+function GameBoard() {
+    return(
+        <div id="board"> Game Board </div>
+        
+    )
+}
+
+export default GameBoard
